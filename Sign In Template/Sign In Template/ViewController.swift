c797554bc7da438b1fc2e931a2ae1b4b66a8ffb4
Michael V. Corpus <mikecorpus@gmail.com>
2017-04-07 12:23:03 +0800

    Initial commit
    Segue ready, basic interface, auto-layout fixed

---
+++ Sign In Template/Sign In Template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sign In Template
+//
+//  Created by Michael V. Corpus on 07/04/2017.
+//  Copyright © 2017 Michael V. Corpus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
